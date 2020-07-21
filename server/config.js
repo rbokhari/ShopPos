588625dae4e50f831ee91ff13a6a786c2b808fd8
Rahman Bokhari <rbokhari@gmail.com>
2020-07-21 14:31:45 -0400

    Update config.js

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ module.exports = {
 
     secret: 'abcasdfkjaslkdfj',
     serverListeningPort: 3090,
-    mongoDBAddress: 'mongodb+srv://admin:gJ8h9qYonG0xprrg@clusterve-fpnnh.mongodb.net/coffee_shop?retryWrites=true&w=majority', // 'mongodb://127.0.0.1:27017/KarakExpress',
+    mongoDBAddress: '', // 'mongodb://127.0.0.1:27017/KarakExpress',
     // mongoDBAddress: 'mongodb://localhost:27017/coffeeshop', // 'mongodb://127.0.0.1:27017/KarakExpress',
     socketIOAddress: '*http://0.0.0.0:3090',
     // role: {
